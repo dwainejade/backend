637ecc522ac952f1df1ee08224f9cae7dd64e957
dwainejade <dwainejade@gmail.com>
2021-01-26 23:27:54 -0500

    initi

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const welcomeRouter = require("./routers/welcome-router");
+
+const cors = require('cors')
+const helmet = require('helmet')
+
+const server = express();
+
+server.use(express.json());
+server.use(helmet())
+server.use(cors())
+server.use("/", welcomeRouter);
+
+server.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).json({
+        message: "Server error? Something isn't right."
+    });
+});
+
+module.exports = server;
