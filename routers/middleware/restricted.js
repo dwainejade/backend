b252af09da2639b721de91c463e456aa1f128fee
KamrynR <62856359+KamrynR@users.noreply.github.com>
2021-01-29 19:26:36 -0600

    header-token

--- routers/middleware/restricted.js
+++ routers/middleware/restricted.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.cookies.token
+    const token = req.headers.token
     if (!token) {
       return res.status(401).json({message: 'token required'})
     }
