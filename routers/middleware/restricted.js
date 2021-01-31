b056878b1554c472fc9877e07ad304636b222523
KamrynR <62856359+KamrynR@users.noreply.github.com>
2021-01-30 22:48:19 -0600

    pr

--- routers/middleware/restricted.js
+++ routers/middleware/restricted.js
@@ -4,13 +4,11 @@ module.exports = async (req, res, next) => {
     try {
         const token = req.headers.token
         if (!token) {
-            console.log('token required')
             return res.status(401).json({message: 'token required'})
         }
 
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-            if (err) {
-                console.log('token invalid')
+            if (err) {  
                 return res.status(401).json({message: 'token invalid'})
             }
             
